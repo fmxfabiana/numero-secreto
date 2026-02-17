c62eef1ba6a886b246907e4e48fe555201075471
fmxfabiana <62762923+fmxfabiana@users.noreply.github.com>
2026-02-17 16:29:54 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,32 @@
+alert('Boas vindas ao jogo do número secreto!');
+let mumeroMaximo = 200;
+let numeroSecreto =  parseInt(Math.random () * mumeroMaximo + 1); // gera um número aleatório entre 1 e numeroMaximo.
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao número secreto.
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${mumeroMaximo}`);
+// se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break; // para o loop        
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1; e a mesma coisa que tentativas ++;
+        tentativas ++;
+        
+    }
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+
+//if (tentativas > 1) {
+//alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+//} else {
+ //   alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!`);
+//}
