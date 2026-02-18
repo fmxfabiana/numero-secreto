a85feacd396f1bd7cee602a3646f3a288cc2b486
fmxfabiana <62762923+fmxfabiana@users.noreply.github.com>
2026-02-18 00:51:24 -0300

    Deixando o jogo mais facil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto!');
-let mumeroMaximo = 300;
+let mumeroMaximo = 50;
 let numeroSecreto =  parseInt(Math.random () * mumeroMaximo + 1); // gera um número aleatório entre 1 e numeroMaximo.
 console.log(numeroSecreto);
 let chute;
